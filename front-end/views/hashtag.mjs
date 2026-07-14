e83b11d6c517f3ccf7d24d4d8834485c426ce61e
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-07-14 19:53:34 +0100

    London | 26-SDC-Mar | Beko | Sprint 1 | hashtag slowing down my browser

--- front-end/views/hashtag.mjs
+++ front-end/views/hashtag.mjs
@@ -17,7 +17,10 @@ import {createHeading} from "../components/heading.mjs";
 function hashtagView(hashtag) {
   destroy();
 
-  apiService.getBloomsByHashtag(hashtag);
+  const tag = hashtag.startsWith("#") ? hashtag : `#${hashtag}`
+  if (state.currentHashtag !== tag) {
+    apiService.getBloomsByHashtag(hashtag);
+  }
 
   renderOne(
     state.isLoggedIn,
